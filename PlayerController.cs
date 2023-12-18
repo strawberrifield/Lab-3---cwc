213ac1c0fd7ed9c3dbaf20c1fad4a725a791a37b
strawberrifield <143817930+strawberrifield@users.noreply.github.com>
2023-12-18 10:12:08 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+    public bool Jump;
+    public bool isOnGround = true;
+    private bool hasJumped = false;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isOnGround && !hasJumped)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            hasJumped = true; // Set hasJumped to true when the player jumps
+        }
+
+        if (isOnGround)
+        {
+            hasJumped = false; // Reset hasJumped when the player is on the ground
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        if ((isFacingRight && horizontal < 0f) || (!isFacingRight && horizontal > 0f))
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
